2045df3b69d384cc4f028050078497ced68311c5
Silver Evans <silverevans13@gmail.com>
2017-08-11 17:02:12 -0400

    Create firewalld.sh

---
+++ tacacs/firewalld.sh
@@ -0,0 +1,7 @@
+sudo firewall-cmd --zone=public --add-service=radius --permanent
+sudo firewall-cmd --zone=public --add-interface=eth0 --permanent
+sudo firewall-cmd --zone=public --add-interface=eth1 --permanent
+sudo firewall-cmd --zone=public --add-port=49/tcp --permanent
+sudo firewall-cmd --reload
+sleep 10
+systemctl restart tac_plus.service
